22d3582abb434513f15b1fd6dcf9bb01b9218f46
Wyatt Cowley <wycowley@gmail.com>
2021-08-15 12:46:51 -0700

    Revert "Adding build again"
    This reverts commit ce87adcdbe10ad40f0f3f600bf15eca51ee7f803.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Checks Dice Probability
+
+Enter in dice rolls for a game duration to see how lucky/unlucky certain games were.
+
---
+++ src/serviceWorker.js
@@ -0,0 +1,12 @@
+/** An empty service worker! */
+self.addEventListener("fetch", function (event) {
+    /** An empty fetch handler! */
+});
+
+self.addEventListener("push", function (event) {
+    event.waitUntil(
+        self.registration.showNotification("Got Push?", {
+            body: "Push Message received",
+        })
+    );
+});
